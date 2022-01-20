c4ad5aa313122a7ad9fdc42bb2dd1fca95e84f57
Samuel Tobia <sam@system76-pc.localdomain>
2022-01-19 19:11:02 -0800

    Server Code Fix: require user is expected to return a function

--- server/src/components/user/middleware.ts
+++ server/src/components/user/middleware.ts
@@ -1,10 +1,12 @@
 
-function requiresUser(req, res, next){
-  if(req.user) return next()
-  next({
-    status: 403,
-    message: "User Forbidden"
-  })
+function requiresUser(){
+  return (req, res, next)=>{
+    if(req.user) return next()
+    next({
+      status: 403,
+      message: "User Forbidden"
+    })
+  }
 }
 
 export {
